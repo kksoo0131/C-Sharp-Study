784ca4771a64bf279fbf07aefb7790411dd60aa3
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-21 01:11:43 +0900

    [unrated] Title: 카드 뭉치, Time: 0.21 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public string solution(string[] cards1, string[] cards2, string[] goal) {
+        int c1count = 0;
+        int c2count = 0;
+
+        foreach (string word in goal)
+        {
+            if (c1count < cards1.Length && cards1[c1count] == word)
+            {
+                c1count += 1;
+            }
+            else if (c2count < cards2.Length &&cards2[c2count] == word)
+            {
+                c2count += 1;
+            }
+            else        
+                return "No";
+        }
+        return "Yes";
+    }
+}
