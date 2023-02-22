a81a7456bed72b162e527a49dcd1ee005124c8f4
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-23 05:04:08 +0900

    [level 0] Title: 모음 제거, Time: 0.44 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.cs
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cs
@@ -3,13 +3,8 @@ using System.Linq;
 
 public class Solution {
     public string solution(string my_string) {
-        string[] vowel = {"a", "e", "i", "o", "u"};
-        
-        foreach (string s in vowel){
-            my_string = my_string.Replace(s , "");
-        }
-        
-        
+        string[] vowel = {"a", "e", "i", "o", "u"};        
+        foreach (string s in vowel) my_string = my_string.Replace(s , "");   
         return my_string;
     }
 }
