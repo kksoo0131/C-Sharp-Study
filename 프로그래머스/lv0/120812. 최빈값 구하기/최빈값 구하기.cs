f07e32e921019c09c048b291d96cfa85bc2c55a6
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-23 04:35:23 +0900

    [level 0] Title: 최빈값 구하기, Time: 11.09 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.cs
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.cs
@@ -3,24 +3,14 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] array) {
-        Array.Sort(array);
+    
+        int[] check = new int[array.Max()+1];
         
-        int array_max = array[array.Length-1];
+        for (int i =0; i < check.Length; i++) check[i] = array.Count(x => x == i);   
         
-        int[] answer_array = new int[array_max+1];
-        
-        for (int i =0; i <=array_max; i++) 
-            answer_array[i] = array.Count(x => x == i);
-        
-        foreach (int i in answer_array){
-            Console.Write($"{i} ");
-        }
-        
-        if (answer_array.Count(x => x == answer_array.Max() ) > 1) return -1;
-        
-        int answer = Array.IndexOf(answer_array, answer_array.Max());
-        
-        return answer;
+        if (check.Count(x => x == check.Max() ) > 1) return -1;
+                
+        return Array.IndexOf(check, check.Max());
          
     }
 }
