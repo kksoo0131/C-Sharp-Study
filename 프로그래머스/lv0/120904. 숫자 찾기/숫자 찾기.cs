5900035bf065ad79cfd3518f01ff11d7c7d28ad3
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-24 02:39:42 +0900

    [level 0] Title: 숫자 찾기, Time: 3.06 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cs
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public int solution(int num, int k) {
-        int answer = -1;
-        foreach (char c in num.ToString()){
-            if (int.Parse(c.ToString()) == k){
-                answer = num.ToString().IndexOf(c) +1;
-            }
-        }
-        return answer;
+        int answer = num.ToString().IndexOf(k.ToString()) +1;
+        return answer == 0 ? -1:answer;
     }
 }
