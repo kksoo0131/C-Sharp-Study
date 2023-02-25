174024d9a1dab2f7c099187b0c7955f5b2790b0b
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-25 16:47:20 +0900

    [level 0] Title: 공 던지기, Time: 0.13 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.cs
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.cs
@@ -2,9 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = (1 + (k-1) * 2) % numbers.Length;
-        return answer==0 ? numbers.Length : answer ;
-        
-        //나머지가 0일때가 
+        return numbers[((k-1) * 2) % numbers.Length];
     }
 }
