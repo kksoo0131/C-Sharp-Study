26cfde401c5635c95d0b608035e0c32df0bb7bb1
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-28 22:58:05 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 4.33 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cs
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cs
@@ -3,13 +3,6 @@ using System.Linq;
 
 public class Solution {
     public string solution(string s) {
-        string answer = "";
-        string list = "abcdefghijklmnopqrstuvwxyz";
-        for (int i=0; i < list.Length; i++){
-            if (s.Count(x => x == list[i]) == 1)
-                answer += list[i];
-        }
-        
-        return answer;
+        return String.Concat(s.Where( x => s.Count(y => x == y) == 1).OrderBy(x => x));
     }
 }
