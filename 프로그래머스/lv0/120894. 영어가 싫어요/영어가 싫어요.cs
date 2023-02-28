c034001aa3529f437ed5319c0441c8b7b490883f
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-28 22:43:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.14 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.cs
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.cs
@@ -2,59 +2,12 @@ using System;
 
 public class Solution { 
     public long solution(string numbers) {
-        string answer = "";
-        for (int i =0; i < numbers.Length;){
-            switch (numbers[i]){
-                case 'z':
-                    i +=4;
-                    answer += "0";
-                    break;
-                case 'o':
-                    i+=3;
-                    answer += "1";
-                    break;
-                case 't':
-                    if (numbers[i+1]== 'w'){
-                        i +=3;
-                        answer += "2";
-                    }
-                    else{
-                        i+=5;
-                        answer += "3";
-                    }
-                    break;
-                case 'f':
-                    if(numbers[i+1]== 'o'){
-                        i+=4;
-                        answer += "4";
-                    }
-                    else{
-                        i+=4;
-                        answer += "5";
-                    }
-                    break;
-                case 's':
-                    if(numbers[i+1]== 'i'){
-                        i+=3;
-                        answer += "6";
-                    }
-                    else{
-                        i+=5;
-                        answer += "7";
-                    }
-                    break;
-                case 'e':
-                    i+=5;
-                    answer += "8";
-                    break;
-                case 'n':
-                    i+=4;
-                    answer += "9";
-                    break;
-                        
-            }
+        string[] number_list = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for (int i=0; i< number_list.Length; i++){
+            numbers =numbers.Replace(number_list[i], i.ToString());
         }
         
-        return Convert.ToInt64(answer);
+        return Convert.ToInt64(numbers);
     }
 }
