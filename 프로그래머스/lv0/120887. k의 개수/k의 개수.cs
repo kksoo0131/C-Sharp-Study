c6291208b235fe53292ef4aec1382e03b6d5ca7e
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-02-28 23:11:06 +0900

    [level 0] Title: k의 개수, Time: 13.67 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.cs
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.cs
@@ -3,11 +3,9 @@ using System.Linq;
 
 public class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        for (int x = i; x <= j; x++){
-            answer += x.ToString().Count(y => y.ToString() == k.ToString());
-        }
-        // i부터 j까지 증가하면서 ToString해서 k를 포함하면 answer +1;
-        return answer;
+      return Enumerable.Range(i, j-i+1)
+            .Select( x => x.ToString().Count(y => y.ToString() == k.ToString()))
+            .Sum();
     }
+
 }
