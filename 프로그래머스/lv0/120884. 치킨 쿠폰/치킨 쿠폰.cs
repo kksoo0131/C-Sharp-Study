78bcd854b3a95e48093b6f2a4e0101b5d4b048ea
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-04 02:33:03 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.cs
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.cs
@@ -3,26 +3,14 @@ using System;
 public class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int temp = 0;
+        int coupon = chicken;
 
-        while (chicken > 0){
-            answer += (chicken / 10);
-            temp += chicken %10;
-            chicken /= 10;
-            
-            if (temp / 10 >= 1){
-                chicken += temp/10;
-                answer += temp/10;
-                temp = temp %10;
-            }
-            Console.WriteLine ($"chicken : {chicken}, temp : {temp}, answer : {answer}");
-            
-            }
-        
-        
-        
-        
-        
+        while (coupon >= 10)
+        {
+            coupon -=10;
+            answer++;
+            coupon++;
+        }
         return answer;
     }
 }
