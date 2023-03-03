851a84b45d70cc14edc5b77a0eb16acf773b6e5c
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-04 06:05:36 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.82 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cs
@@ -1,49 +1,32 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class Solution {
-    static int answer =0;
     public int solution(int[,] lines) {
-         
-        int[] first_overlap1 = Overlap(lines[0,0],lines[0,1],lines[1,0], lines[1,1]);
-        int[] first_overlap2 = Overlap(lines[0,0],lines[0,1],lines[2,0], lines[2,1]);
-        int[] first_overlap3 = Overlap(lines[1,0],lines[1,1],lines[2,0], lines[2,1]);        
+        // 길이가 1이상인것만 취급해야하고
+        //중첩되는걸 한번만 잡아내야되는 문제.
+        List <int[]> listline = new List<int[]>();
+        List <int[]> answer = new List<int[]>();
         
-        answer += first_overlap1[1] - first_overlap1[0];
-        answer += first_overlap2[1] - first_overlap2[0];
-        answer += first_overlap3[1] - first_overlap3[0];
-        
-        int[] second_overlap1 =
-            Overlap(first_overlap1[0], first_overlap1[1],first_overlap2[0], first_overlap2[1]);
-        int[] second_overlap2 =
-            Overlap(first_overlap1[0], first_overlap1[1],first_overlap3[0],first_overlap3[1]);
-        int[] second_overlap3 =
-            Overlap(first_overlap2[0], first_overlap2[1],first_overlap3[0],first_overlap3[1]);
-        
-    
-        answer -= second_overlap2[1] - second_overlap2[0];
-        answer -= second_overlap3[1] - second_overlap3[0];
-        
-        Console.WriteLine();
-        
-        
-
-        
-       
-     
-        // 두 선분이 겹치는 좌표를 확인
-        // 1,2  2,3  1,3 모두 확인을 하고
-        // 한번 더 겹치는 좌표를 확인 해서 2.번에서 뺴준다.
-        
+        for (int i =0; i<lines.GetLength(0); i++){
             
-        
-        return answer;
-    }
-    
-    static int[] Overlap(int a0, int a1, int b0, int b1){
-        if (Math.Max(a0,b0) > Math.Min(a1,b1))
-            return new int[]{0,0};           
-        return new int[]{Math.Max(a0,b0), Math.Min(a1,b1)};
-        
+            int max = Math.Max(lines[i,0],lines[i,1]);
+            int min = Math.Min(lines[i,0],lines[i,1]);
+            
+            for(int j =min; j<max; j++){
+                if (listline.Exists (x => x[0] ==j && x[1] == j+1)){
+                    if (!answer.Exists (x => x[0] ==j && x[1] == j+1)){
+                        answer.Add(new int[] {j,j+1});
+                    }
+                }
+                else{
+                    listline.Add(new int[] {j,j+1});
+                }
+                    
+            }
+            
+        }
+        return answer.Count();
     }
 }
