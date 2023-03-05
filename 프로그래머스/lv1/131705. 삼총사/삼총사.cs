f0b4179f32347cc012b4c11d57840eafe2989fff
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-06 04:43:41 +0900

    [level 1] Title: 삼총사, Time: 4.03 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cs
@@ -3,21 +3,30 @@ using System.Linq;
 using System.Collections.Generic;
 
 public class Solution {
+    public List <int> answer;
+    public int[] number;
     public int solution(int[] number) {
-        List <int> list = number.ToList();
-        int answer = 0;
+        answer = new List<int>();
+        this.number = number;
+        DFS(0, 0 , 0);
+        return answer.Where(x => x==0).Count() ;
+    }
+    
+    public void DFS(int start, int depth, int sum){
         
-        int temp = 0;
-        while (list.Count != 0){
-            temp = list[0];
-            list.RemoveAt(0);
-            for(int i =0; i < list.Count; i++){
-                for (int j= i+1; j <list.Count; j++ ){
-                    if(temp + list[i] +list[j] == 0)
-                        answer += 1;
-                }
-            }
+        if (depth ==3){
+            answer.Add(sum);
+            Console.WriteLine();
+            return;
         }
-        return answer;
+        
+        for (int i= start; i<number.Length; i++){
+            Console.Write($"{number[i]} + ");
+            sum += number[i];
+            DFS(i+1, depth+1, sum);
+            sum -= number[i];
+            }    
+        
+        
     }
 }
