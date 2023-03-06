83f6a351c2c4f63830ec1a1e7cb68787a423eaec
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-07 01:12:44 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cs
@@ -1,28 +1,25 @@
 using System;
 
 public class Solution {
-    public int[] numbers;
-    public int answer;  
+    public int[] numbers; 
     public int target;
     
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
-        this.answer =0;
         this.target = target;
-        DFS (0,0);
-        return answer;
+       
+        return  DFS (0,0);
     }
     
-    public void DFS(int start, int sum){
+    public int DFS(int start, int sum){
         
         if (start == numbers.Length){
             if(sum == target)
-                answer +=1;
-            return;
+                return 1;
+            return 0;
         }
+        else
+            return DFS(start +1, sum + numbers[start]) + DFS(start +1, sum - numbers[start]);
                 
-        DFS(start +1, sum + numbers[start]);
-        DFS(start +1, sum - numbers[start]);
-        
     }
 }
