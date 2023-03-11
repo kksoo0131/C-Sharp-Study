414bfca14b0e5c8e26db00446671f46be319a012
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-11 17:44:51 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.87 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
@@ -9,13 +9,7 @@ public class Solution {
                 answer += " ";
                 continue;
             }
-            
-            //첫 문자 숫자 방지
-            if (int.TryParse(ss[0].ToString(), out _)){
-                answer += ss.ToLower()+" ";
-            }
-            else    
-                answer += char.ToUpper(ss[0]) + ss.Substring(1).ToLower()+" ";          
+            answer += char.ToUpper(ss[0]) + ss.Substring(1).ToLower()+" ";          
         }       
         // 마지막 공백문자 제거
         return answer.Substring(0,answer.Length-1);
