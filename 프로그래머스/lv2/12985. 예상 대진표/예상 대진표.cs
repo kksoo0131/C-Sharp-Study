ae8c90010440d593e8eaedf974dd690a61bfc4d8
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-16 01:32:39 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cs
@@ -1,39 +1,18 @@
 using System;
-using System.Collections.Generic;
+
 class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int round =1;
-        int match =2;           
-        
-        while (true){
-            //그룹을 나눠서 확인. 1~ 16 17 ~32
-            for (int i = 1; i <= n; i+=match){
-                bool checkA = false;
-                bool checkB = false;
-                
-                // 그룹에 A,B둘다있다면 리턴
-                for (int j =0; j < match ; j++){
-                    if (i + j == a)
-                        checkA = true;
-                    
-                    if (i + j == b)
-                        checkB = true;
-                    
-                    if (checkA == true && checkB == true)
-                        return round;    
-                }
-                
-                
-            }
-            
-            // 다음 라운드로.
-            match *=2;
-            round++;
-                
+        int answer =0;
+        // 대진표를 한층씩 올라가서 만난다.
+        // a = b 라는 말은 그전 대진에서 a와 b가 만났다는 뜻.
+        while ( a != b){
+            a = a/2 + a%2;
+            b = b/2 + b%2;
+            answer++;
         }
-
-        return round;
+        
+        return answer;
     }
 }
