ba49dbc26a78d7bdc7791bddfec3a1f9296d1616
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-03-21 20:09:25 +0900

    [level 2] Title: 멀리 뛰기, Time: 1.77 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 public class Solution {
     Dictionary<int, int> dic = new Dictionary<int, int>();
+    
     public int solution(int n) {    
         dic.Add(1,1); 
         dic.Add(2,2); 
@@ -13,4 +14,7 @@ public class Solution {
             dic.Add(n, fibo(n-2) + fibo(n-1));
         return dic[n] % 1234567;
     }
+    // n에 도착하는 방법은 
+    // n-1 지점에서 한칸 뛰는 경우, n-2 지점에서 두칸 뛰는 경우로 두가지 방법이 있다.
+    // 그러므로 n-1 에 도착하는 방법의 수 + n-2 에 도착하는 방법의 수가 n의 도착하는 방법의 수가 된다.
 }
