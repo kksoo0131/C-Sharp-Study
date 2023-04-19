9c09faa6c5b7baa79fdce0af5ed958e99a701c71
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-04-20 08:56:07 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void recursive(int n,int repeat ) {
+    string temp = "";
+    for (int i = 1; i <= repeat * 4; i++) {
+        temp += "_";
+    }
+
+    cout << temp +"\"재귀함수가 뭔가요?\"\n";
+    
+    if (n == 0) {
+        cout << temp +"\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+    }
+    else {
+        cout << temp + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n"
+            << temp + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n"
+            << temp + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+        recursive(n - 1,repeat+1);
+    }
+    cout << temp+"라고 답변하였지.\n";
+        
+    
+    
+
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int N;
+    cin >> N;
+    cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+    recursive(N,0);
+}
