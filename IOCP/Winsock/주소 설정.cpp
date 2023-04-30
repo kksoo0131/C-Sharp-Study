368ebef60c7da1d88babe5d15a34233d6bee3da1
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-01 06:42:38 +0900

    Update 주소 설정.cpp

--- IOCP/Winsock/주소 설정.cpp
+++ IOCP/Winsock/주소 설정.cpp
@@ -10,6 +10,11 @@ void ServerAddrSetting(int BindPort){
   _serverAddr.sin_family = AF_INET;
   _serverAddr.sin_port = htons(BindPort);
   _serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  
-  // INADDR_ANY 어떤주소라도 접속을 받아들이겠다라는 의미.
 }
+// INADDR_ANY 어떤주소라도 접속을 받아들이겠다라는 의미.
+  
+//htons() unsigned short 
+//htonl() unsigned long
+//다양한 플랫폼과 네트워크 환경에서 호환성을 유지하기 위해서 Big-endian으로 변경해준다.
+//대부분의 네트워크 장비들은 Big-endian방식을 사용하고, 일반적인 컴퓨팅 환경은 Little-endian방식을 사용한다.
+
