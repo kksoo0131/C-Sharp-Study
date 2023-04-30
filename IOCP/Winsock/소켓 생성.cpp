d4da2ca6f94c2baaa7cf7218a0b48f647223eac9
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-01 06:24:05 +0900

    Update 소켓 생성.cpp

--- IOCP/Winsock/소켓 생성.cpp
+++ IOCP/Winsock/소켓 생성.cpp
@@ -1,4 +1,13 @@
 #include <winsock2.h>
+// winsock2.h에 포함
+SOCKET WSASocket(                     //소켓 객체 생성
+  int                af,              //주소체계 AF_INET, AF_INET6
+  int                type,            //소켓 유형 SOCK_STREAM, SOCK_DGRAM 
+  int                protocol,        //프로토콜 IPPROTO_TCP, IPPROTO_UDP
+  LPWSAPROTOCOL_INFO lpProtocolInfo,  //프로토콜 정보 NULL
+  GROUP              g,               //멀티캐스트 소켓 그룹 0
+  DWORD              dwFlags          //옵션을 지정 (WSA_FLAG_OVERLAPPED 비동기식 소켓 생성)
+);
 
 SOCKET _listenSocket = INVALID_SOCKET;
 
