1656267a40bd7c8ca8bb04bd4d2685f05899570f
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-02 06:45:43 +0900

    Update and rename 워커 스레드 생성.cpp to 스레드 생성.cpp

--- IOCP/std::thread/스레드 생성.cpp
+++ IOCP/std::thread/스레드 생성.cpp
@@ -4,7 +4,7 @@
 #define MAX_WORKERTHREAD 4 // 쓰레드 풀에 넣을 쓰레드 수
 
 std::vector<std::thread> _IOWorkerThreads;
-std::thread _AccepterThread;
+std::thread _accepterThread;
 _isWorkerRun = true;
 
 //WaitingThread Queue에 대기 상태로 넣을 쓰레드 생성 권장 개수 : cpu개수 *2+1
@@ -19,3 +19,7 @@ void CreateWorkerThread()
 	
 }
 
+void CreateAccepterThread()
+{
+	_accepterThread = std::thread([this]() { AccepterThread(); });
+}
