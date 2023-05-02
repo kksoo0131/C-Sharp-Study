896a62258ca12475ebd18d1bd94b48dcff4abbfa
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-03 04:44:59 +0900

    Update 스레드 파괴.cpp

--- IOCP/std::thread/스레드 파괴.cpp
+++ IOCP/std::thread/스레드 파괴.cpp
@@ -1,24 +1,18 @@
-void DestroyThread()
-{ 
-  _isWorkerRun = false;
-  for (auto& th : _IOWokerThreads)
-  {
-    if(th.joinable())
-    {
-      th.join();
-    }
-  }
-  
-  _isAccepterRun = false;
-  if (_accepterThread.joinable())
-  {
-    _accepterThread.join();
-  }
-  
-  CloseHandle(_IOCPHandle);
-  closesocket(_listenSocket);
-  
-}
+void destroyThread() {
+		_isWorkerRun = false;
+		CloseHandle(_IOCPHandle);// 미리 끊어줘야 오류가 발생해서 스레드를 멈춤
+		for (auto& th : _workerThreads) {
+			if (th.joinable()) {
+				th.join();
+			}
+		}
+
+		_isAccepterRun = false;
+		closesocket(_listenSocket);// 미리 끊어줘야 오류가 발생해서 스레드를 멈춤
+		if (_accepterThread.joinable()) {
+			_accepterThread.join();
+		}
+	}
 // joinable() 스레드 객체의 상태를 확인하는 함수, join()함수를 호출 할 수 있는지 여부를 반환 할수없다면 true
 
 // join() 호출된 스레드가 종료될 때까지 대기하는 함수, 레드가 종료될 때까지 현재 스레드가 대기하다 반환된다.
