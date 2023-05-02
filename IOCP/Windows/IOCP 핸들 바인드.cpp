5d896cc8cb126c84075e1b76054bd1ab795ad515
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-02 11:32:54 +0900

    Update IOCP 핸들 바인드.cpp

--- IOCP/Windows/IOCP 핸들 바인드.cpp
+++ IOCP/Windows/IOCP 핸들 바인드.cpp
@@ -1,19 +1,16 @@
+//CompletionPort객체와 소켓과 CompletionKey를 연결시키는 역할을 한다.
+	bool BindIOCompletionPort(ClientInfo* pClientInfo)
+	{
+		//socket과 pClientInfo를 CompletionPort객체와 연결시킨다.
+		auto hIOCP = CreateIoCompletionPort((HANDLE)pClientInfo->m_socketClient
+										, _IOCPHandle
+										, (ULONG_PTR)(pClientInfo), 0);
+		
+		if (NULL == hIOCP || mIOCPHandle != hIOCP)
+		{
+			printf("[에러] CreateIoCompletionPort()함수 실패: %d\n", GetLastError());
+			return false;
+		}
 
-HANDLE BindIoCompletionPort(          // IOCP 핸들과 핸들(소켓)을 연결
-  HANDLE    FileHandle,               // IOCP 핸들
-  HANDLE    CompletionKey,            // 소켓 컨텍스트(핸들) 정보와 연결할 완료 키 
-  ULONG_PTR CompletionValue,          // 완료 패킷에 대한 컨텍스트 정보
-  DWORD     NumberOfConcurrentThreads // 작업처리 쓰레드 수
-);
-
-bool BindSocket(ClientInfo* pClientInfo)
-{
-  auto hIOCP = BindIoCompletionPort(_IOCPHandle, (HANDLE)pClientInfo->_socketClient, (ULONG_PTR)(pClientInfo), 0);
-  
-  if (NULL= hIOCP || _IOCPHANDLE != hIOCP)
-  {
-    printf("BindIoCompletionPort() 실패 : %d\n",GetLastError());
-    return false;
-  }
-  return true;
-}
+		return true;
+	}
