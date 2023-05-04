0ee9318e121012a0d0deb4bcecc011b3f6a76743
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-04 11:52:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 116 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 연결된 노드중 나보다 1에 가까운게 내부모임
+// 1부터 한번 순회하면서 깊이를 지정해준다?
+
+vector<int> arr[100001];
+int depth[100001];
+
+void dfs(int N, int d) {
+	depth[N] = d;
+
+	for (int i : arr[N]) {
+		if (depth[i] == 0) {
+			dfs(i, d + 1);
+		}
+	}
+
+}
+
+int main() {
+	int N; 
+	cin >> N;
+
+	
+
+	for (int i = 1; i <= N - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		arr[a].push_back(b);
+		arr[b].push_back(a);
+	}
+
+	dfs(1, 1);
+	for (int i = 2; i <= N; i++) {
+		int answer = i;
+		for (int j : arr[i]) {
+			if (depth[j] < depth[answer]) {
+				answer = j;
+			}
+		}
+
+		cout << answer << '\n';
+	}
+
+}
+
+
+//1 6 3 5
+//4 7
+//2
