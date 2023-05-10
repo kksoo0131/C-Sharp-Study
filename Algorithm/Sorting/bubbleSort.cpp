86fbd959c639d1e5d59e01262da0008b028882e6
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-11 06:20:47 +0900

    Update bubbleSort.cpp

--- Algorithm/Sorting/bubbleSort.cpp
+++ Algorithm/Sorting/bubbleSort.cpp
@@ -7,16 +7,21 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& a, int& b) {
+    if (a > b) {
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+}
 
 int main(){
   int arr[10] = {9,8,7,6,5,4,3,2,1,0};
   
-  for (int i =1; i<= arr.size(); i++){
+  for (int i =1; i<= 10; i++){
     for (int j=0; j < 10-i; j++){
       if (arr[j] > arr[j+1]){
-        int temp = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = temp;
+        swap(arr[j], arr[j+1);
       }
     }
   }
