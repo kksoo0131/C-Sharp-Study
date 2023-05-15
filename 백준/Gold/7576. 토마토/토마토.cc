ba0fcf7e8e264b16e937546fd239aabbf4feb36a
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-16 00:09:48 +0900

    [Gold V] Title: 토마토, Time: 84 ms, Memory: 10268 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -7,46 +7,31 @@ int dx[4] = {0,0,-1,1};
 int N, M;
 int answer = 0;
 int** arr;
-int** visited;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0), cout.tie(0);
 	cin >> M >> N;
 
-	arr = new int*[N];
-	
+	deque<pair<int, int>> dq;
+	arr = new int* [N];
 	for (int i = 0; i < N; i++) {
 		arr[i] = new int[M];
 		for (int j = 0; j < M; j++) {
 			cin >> arr[i][j];
-		}	
-	}
-
-
-	deque<pair<int, int>> dq;
-	visited = new int* [N];
-	for (int i = 0; i < N; i++) {
-		visited[i] = new int[M];
-		for (int j = 0; j < M; j++) {
 			if (arr[i][j] == 1) {
 				dq.push_back({ i,j });
-				visited[i][j] = 1;
 			}
-				
-			else
-				visited[i][j] = 0;
 		}
-		
 	}
-	
+
 	while (!dq.empty()) {
 		int y = dq.front().first;
 		int x = dq.front().second;
 		dq.pop_front();
 
-		if (visited[y][x] > answer) {
-			answer = visited[y][x];
+		if (arr[y][x] > answer) {
+			answer = arr[y][x];
 		}
 
 		for (int i = 0; i < 4; i++) {
@@ -55,14 +40,9 @@ int main() {
 			if (ny < 0 || ny >= N || nx < 0 || nx >= M)
 				continue;
 
-			if (visited[ny][nx] == 0) {
-
-				if (arr[ny][nx] == 0) {
-					arr[ny][nx] = 1;
-					visited[ny][nx] = visited[y][x] + 1;
-					dq.push_back({ ny, nx });
-				}
-			
+			if (arr[ny][nx] == 0) {
+				arr[ny][nx] = arr[y][x] + 1;
+				dq.push_back({ ny, nx });		
 			}
 		}
 	}
