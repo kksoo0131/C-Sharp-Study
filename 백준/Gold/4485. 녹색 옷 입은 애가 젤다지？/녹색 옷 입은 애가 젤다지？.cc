32187df3a968d934462c598756dbec2ac6f81744
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-16 09:59:37 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 496 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -0,0 +1,103 @@
+// 0 ~ N-1 칸까지 최소비용으로 이동하는 문제
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int N;
+int dy[4] = { -1,1,0,0 };
+int dx[4] = { 0,0,-1,1 };
+int** arr;
+bool** visited;
+int** dist;
+const int MAX_VALUE = 999999999;
+
+void Clear() {
+	for (int i = 0; i < N; i++) {
+		delete[] arr[i];
+		delete[] visited[i];
+		delete[] dist[i];
+	}
+
+	delete[] arr;
+	delete[] visited;
+	delete[] dist;
+}
+void Input(int N) {
+	arr = new int* [N];
+	visited = new bool* [N];
+	dist = new int* [N];
+
+	for (int i = 0; i < N; i++) {
+		arr[i] = new int[N];
+		visited[i] = new bool[N];
+		dist[i] = new int[N];
+
+		for (int j = 0; j < N; j++) {
+			cin >> arr[i][j];
+			visited[i][j] = false;
+			dist[i][j] = MAX_VALUE;
+		}
+	}
+}
+void Cave(pair<int,int> pos) {
+	int y = pos.first;
+	int x = pos.second;
+	visited[y][x] = true;
+
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i], nx = x + dx[i];
+
+		if (ny < 0 || ny >= N || nx < 0 || nx >= N)
+			continue;
+
+		if (!visited[ny][nx]) {
+			if (dist[ny][nx] > dist[y][x] + arr[ny][nx])
+				dist[ny][nx] = dist[y][x] + arr[ny][nx];
+		}		
+	}
+}
+
+// 최단거리중 최소인것을 찾아서 다음으로 넘어간다.
+pair<int,int> SelectShortDist() {
+	int minDist = MAX_VALUE;
+	pair<int, int> minPos = { -1,-1 };
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (!visited[i][j] && dist[i][j] != MAX_VALUE) {
+				if (minDist > dist[i][j]) {
+					minDist = dist[i][j];
+					minPos = { i,j };
+				}
+			}
+		}
+	}
+
+	return minPos;
+}
+
+int main() {
+	int answerNumber = 1;
+	while (true) {
+		cin >> N;
+		if (N == 0)
+			break;
+		Input(N);
+
+		dist[0][0] = arr[0][0];
+		Cave({ 0, 0 });
+
+		while (true) {
+			pair<int, int> nextPos = SelectShortDist();
+
+			if (nextPos.first == -1) {
+				break;
+			}
+
+			Cave(nextPos);
+		}
+		cout << "Problem " << answerNumber++ << ": " <<dist[N-1][N-1] << '\n';
+		Clear();
+		
+	}
+}
