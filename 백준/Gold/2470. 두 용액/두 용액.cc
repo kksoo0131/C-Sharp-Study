72252ef8b78c33d8280b774fc3e12ff62e235660
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-19 20:47:06 +0900

    [Gold V] Title: 두 용액, Time: 68 ms, Memory: 2424 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#define INF 2000000000
+
+using namespace std;
+
+deque<int> answer(2);
+
+int main() {
+	int N;
+	cin >> N;
+	deque<int> arr(N);
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+
+	int start = 0;
+	int end = N - 1;
+	int min = INF;
+
+	while (start < end) {
+		int sum = arr[start] + arr[end];
+
+		if (min > abs(sum)) {
+			min = abs(sum);
+			answer[0] = arr[start];
+			answer[1] = arr[end];
+
+			if (sum == 0)
+				break;
+		}
+
+		if (sum < 0)
+			start++;
+		else
+			end--;
+	}
+
+	cout << answer[0] << " " << answer[1];
+}
