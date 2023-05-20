19c7341a40601d46311e3f8ff03acefe2aeb9e8c
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-20 22:18:26 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2556 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -1,42 +1,49 @@
-
 #include <iostream>
-#include <deque>
 #include <algorithm>
-#define INF 2000000000
+#include <deque>
 
 using namespace std;
 
-deque<int> answer(2);
-
 int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
 	int N;
 	cin >> N;
+
 	deque<int> arr(N);
+	deque<int> answer(2);
+
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
+
 	sort(arr.begin(), arr.end());
 
 	int start = 0;
 	int end = N - 1;
-	int min = INF;
+	int minValue = 2000000001;
 
 	while (start < end) {
 		int sum = arr[start] + arr[end];
 
-		if (min > abs(sum)) {
-			min = abs(sum);
+		if (minValue > abs(sum)) {
+			minValue = abs(sum);
 			answer[0] = arr[start];
 			answer[1] = arr[end];
-
-			if (sum == 0)
-				break;
 		}
 
-		if (sum < 0)
+
+		if (sum == 0) {
+			break;
+		}
+		else if (sum < 0) {
 			start++;
-		else
+		}
+		else {
 			end--;
+		}
 	}
 
 	cout << answer[0] << " " << answer[1];
