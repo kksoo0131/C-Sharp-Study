baa34af49d3f360412072799320fb5a25bd9c756
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-24 20:10:57 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 132 ms, Memory: 6584 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,36 +1,50 @@
+//그래프를 입력 받아서
+//연결 요소의 개수를 출력
+//몇개로 나누어져 있는지 인가?
 #include <iostream>
-#include <vector>
+#include <deque>
+#include <algorithm>
+
 using namespace std;
 
+void DFS( deque<int>* arr, bool* visited, int n) {
 
-int N, M, answer = 0;
-vector<int> vec[1001];//아 벡터 배열;;;;
-vector<bool> check(1001,true);
-
-void DFS(int N) {
-	
-		check[N] = false;
-		for (int i : vec[N]) {
-			if (check[i]) { DFS(i); }		
-		}
-	
-}
-
-int main() {
-	cin >> N >> M;
-
-	for (int i = 1; i <= M; i++) {
-		int start, end;
-		cin >> start >> end;
-		vec[start].push_back(end);
-		vec[end].push_back(start);
+	if (visited[n]) return;
+	visited[n] = true;
+	for (int i : arr[n]) {
+		DFS(arr, visited, i);
 	}
 
+
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int N, M;
+	cin >> N >> M;
+
+	deque<int>* arr = new deque<int>[N + 1];
+
+	for (int i = 0; i < M; i++) {
+		int tA, tB;
+		cin >> tA >> tB;
+
+		arr[tA].push_back(tB);
+		arr[tB].push_back(tA);
+	}
+
+	for (int i = 0; i <= N; i++) {
+		sort(arr[i].begin(), arr[i].end());
+	}
+
+	bool* visited = new bool[N + 1];
+	fill(visited, visited + N + 1, false);
+	int answer = 0;
 	for (int i = 1; i <= N; i++) {
-		if (check[i]) {
-			answer++; // 첫입장때만 늘려줌
-			DFS(i);
-		}
+		if (visited[i]) continue;
+		DFS(arr, visited, i);
+		answer++;
 	}
 
 	cout << answer;
