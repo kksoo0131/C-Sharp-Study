8c2dd36bc61f55a2c2bdff8fd5cfc5fc03136b4c
김광수 <99727193+kksoo0131@users.noreply.github.com>
2023-05-25 11:04:35 +0900

    [Gold V] Title: 인구 이동, Time: 644 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,98 +1,101 @@
-// 옆나라와의 인구차이가 L이상 R이하라면 국경이 연결된다.
-// 연합의 인구수를 모두 더해서 나눠가진다.
+//몇일동안 인구이동을 해야하는가?
+// 
+// 각 나라들의 인구를 비교하며
+// 완전탐색으로 연합을 구성한다.
 
+// 시작좌표가 1,1인가?
 #include <iostream>
 #include <deque>
-#include <cmath>
+
 using namespace std;
 
 int N, L, R;
-int arr[50][50];
-int dy[4] = {-1,1,0,0};
-int dx[4] = {0,0,-1,1};
-int date = 0;
+int arr[51][51];
+int dy[4] = { -1,1,0,0 };
+int dx[4] = { 0,0,-1,1 };
 
-//연결되있는 칸에 접근을 할수 있게 저장해둬야됨
-// 이번 턴에 봤는지 1.
-// 이번 연합에 있었는지 1.
+int BFS(bool(&visited)[51][51], int y, int x) {
+	deque<pair<int, int>> _unions;
+	deque<pair<int, int>> dq;
 
-deque<pair<int, int>> bfs(pair<int,int> start, int(&visited)[50][50]) {
-	deque<pair<int, int>> bfsdq;
-	deque<pair<int, int>> unite;
+	_unions.push_back({ y,x });
+	dq.push_back({ y,x });
 
-	unite.push_back({ 0,0 }); // 0은 연합의 인원수, 1은 연합의 인구수
-	bfsdq.push_back(start);
-	
-	visited[start.first][start.second] = 1;
+	visited[y][x] = true;
+	int sum = arr[y][x];
 
-
-	while (!bfsdq.empty()) {
-		int y = bfsdq.front().first, x = bfsdq.front().second;
-		bfsdq.pop_front();
-
-		unite.push_back({y,x});
-		unite.front().first++;
-		unite.front().second += arr[y][x];
+	while (!dq.empty()) {
+		int Y = dq.front().first, X = dq.front().second;
+		dq.pop_front();
 
 		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i], nx = x + dx[i];
-			if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+			int ny = Y + dy[i], nx = X + dx[i];
 
-			int difference = abs(arr[ny][nx] - arr[y][x]);
-			if (visited[ny][nx] == 0 && difference >= L && difference <= R) {
-				bfsdq.push_back({ny,nx});
-				visited[ny][nx] = visited[y][x] + 1;
+			if (ny < 1 || ny > N || nx <1 || nx > N)
+				continue;
+
+			int diff = abs(arr[Y][X] - arr[ny][nx]);
+			if (diff >= L && diff <= R && !visited[ny][nx]) {
+				visited[ny][nx] = true;
+				dq.push_back({ ny, nx });
+				_unions.push_back({ ny, nx });
+				sum += arr[ny][nx];
 			}
 		}
 	}
 
-	return unite;
-}
-
-bool movement() {
-	int visited[50][50] = { 0 };
-	deque<deque<pair<int, int>>> unite_dq;
-	int uniteNum = 0;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			
-			if (visited[i][j] == 0) {	
-				unite_dq.push_back(bfs({ i,j }, visited));
-			}
-		}
+	if (_unions.size() == 1) {
+		return 0;
 	}
+	else {
+		int avg = sum / _unions.size();
 
-	for (deque<pair<int, int>> unite : unite_dq) {
-		int num = unite.front().first;
-		if (num > 1) uniteNum++;
-		int sum = unite.front().second;
-		int avg = sum / num;
-		unite.pop_front();
-		for (pair<int, int> p : unite) {
+		for (pair<int, int> p : _unions) {
 			arr[p.first][p.second] = avg;
 		}
+
+		return 1;
 	}
-
-	if (uniteNum == 0)
-		return false;
-	return true;
+	
+	
 }
-
 int main() {
-	
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
 	cin >> N >> L >> R;
-	
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
 			cin >> arr[i][j];
 		}
 	}
 
-	while (movement()) {
-		date++;
-	};
+	int answer = 0;
 
-	cout << date;
+	while (true) {
+		
+		bool visited[51][51];
+
+		for (int i = 1; i <= 50; i++) {
+			for (int j = 1; j <= 50; j++) {
+				visited[i][j] = false;
+			}
+
+		}
+
+		int swap = 0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (!visited[i][j]) {
+					swap += BFS(visited, i, j);
+				}
+			}
+		}
+
+		if (swap == 0)
+			break;
+		answer++;
+	}
+
+	cout << answer;
 }
