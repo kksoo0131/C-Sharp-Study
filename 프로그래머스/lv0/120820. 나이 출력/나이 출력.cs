bf5c817271af561177acdce791d79492b2f7d3ef
김광수 <a67636521@gmail.com>
2023-07-21 20:27:41 +0900

    [level 0] Title: 나이 출력, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.cs
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.cs
@@ -2,8 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int age) {
-        int answer = 0;
-        answer = 2022 - age +1;
-        return answer;
+        return 2022 - age +1;
     }
 }
