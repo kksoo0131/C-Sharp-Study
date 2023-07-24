4599a12fadf2242f693dd307cae177c315ac17b4
김광수 <a67636521@gmail.com>
2023-07-24 18:54:11 +0900

    [level 0] Title: 나머지 구하기, Time: 0.13 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.cs
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 % num2;
-        return answer;
+        return num1 % num2;
     }
 }
