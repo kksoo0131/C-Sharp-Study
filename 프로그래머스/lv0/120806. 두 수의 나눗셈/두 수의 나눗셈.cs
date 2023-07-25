b5e77c42ce65ccd02deb2e64680285464dbb288a
김광수 <a67636521@gmail.com>
2023-07-25 17:13:28 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -1,8 +1,8 @@
 using System;
 
 public class Solution {
-    public int solution(int num1, int num2) {
-       
-        return (int)((float)num1 / (float)num2 * 1000);
+    public int solution(int num1, int num2) {       
+        //return (int)((float)num1 / (float)num2 * 1000);
+        return num1 * 1000 / num2;
     }
 }
