307549d2f9b9af9786c6ce68e2677975021e931a
김광수 <a67636521@gmail.com>
2023-07-26 17:14:51 +0900

    [level 0] Title: 각도기, Time: 0.17 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.cs
+++ 프로그래머스/lv0/120829. 각도기/각도기.cs
@@ -2,8 +2,17 @@ using System;
 
 public class Solution {
     public int solution(int angle) {
-        int answer = 1 + angle / 90 + (angle > 90 ? 1 : 0);
+        int answer = angle / 90;
+        int R = angle % 90;
+        
+        if (answer == 2)
+            return 4;
+        else if (answer ==1 && R != 0)
+            return 3;
+        else if (answer == 1)
+            return 2;
+        else
+            return 1;
 
-        return answer;
     }
 }
