d0f65b9fa95f2a7197c922d837f887ca51e28125
김광수 <a67636521@gmail.com>
2023-07-28 17:22:00 +0900

    [level 0] Title: 배열의 유사도, Time: 0.56 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.cs
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.cs
@@ -3,6 +3,11 @@ using System.Linq;
 
 public class Solution {
     public int solution(string[] s1, string[] s2) {
-        return s1.Count(x => s2.Contains(x));
+        int answer = 0;
+        
+        foreach(string str in s1){
+            answer += s2.Contains(str) ? 1 : 0;
+        }
+        return answer;
     }
 }
