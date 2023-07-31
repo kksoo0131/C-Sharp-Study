40a54a3999889897d570bd0f25f89fbd4643ab97
김광수 <a67636521@gmail.com>
2023-07-31 20:51:54 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 1.37 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -3,7 +3,6 @@ using System;
 public class Solution {
     public int solution(int[] sides) {
         Array.Sort(sides);
-        int answer = sides[2] < sides[1] + sides[0] ? 1 : 2 ;    
-        return answer;
+        return sides[2] < sides[1] + sides[0] ? 1 : 2;
     }
 }
