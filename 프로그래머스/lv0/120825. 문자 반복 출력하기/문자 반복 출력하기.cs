c3abc28a63fde719bf4e0c5f030ea077bcb0447b
김광수 <a67636521@gmail.com>
2023-08-02 16:27:33 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.29 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.cs
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.cs
@@ -3,8 +3,11 @@ using System;
 public class Solution {
     public string solution(string my_string, int n) {
         string answer = "";
-        foreach (char c in my_string){            
-                answer += new string(c,n); 
+        
+        foreach(char c in my_string){
+            for(int i = 0; i< n; i++){
+                answer += c;        
+            }
         }
         return answer;
     }
