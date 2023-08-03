e52f30e92edd75e4ce45ed35fecc9220122321b5
김광수 <a67636521@gmail.com>
2023-08-03 16:45:14 +0900

    [level 0] Title: 모음 제거, Time: 0.58 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.cs
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cs
@@ -1,10 +1,14 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public string solution(string my_string) {
-        string[] vowel = {"a", "e", "i", "o", "u"};        
-        foreach (string s in vowel) my_string = my_string.Replace(s , "");   
+        string vowel = "aeiou";
+        
+        foreach (char c in vowel){
+            my_string = my_string.Replace(c.ToString(), "");
+        }
+        
+        
         return my_string;
     }
 }
