01ad2a90c86b1f1869b255462963547398fc2b90
김광수 <a67636521@gmail.com>
2023-08-04 19:48:17 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.56 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.cs
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.cs
@@ -1,9 +1,17 @@
 using System;
 
 public class Solution {
+    
     public int solution(int[] array, int n) {
+        /*
+        int answer = 0;
+        foreach(int i in array){
+            answer += (i == n ? 1 : 0);
+        }
+        return answer;
+        */
         
-  
-        return Array.FindAll(array, x => x == n ).Length;
+        return Array.FindAll(array, x => x == n).Length;
     }
+   
 }
