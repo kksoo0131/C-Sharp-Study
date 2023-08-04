02f1d635e588e276535da73f4f705adc299cee9a
김광수 <a67636521@gmail.com>
2023-08-04 19:57:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.18 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -3,19 +3,23 @@ using System;
 public class Solution {
     public int solution(int price) {
         
+        /*
+        float rate;
         if (price >= 500000){
-            return (int)(price * 0.8f);
+            rate = 0.8f;
+        }
+        else if (price >= 300000){
+            rate = 0.9f;
+        }
+        else if (price >= 100000){
+            rate = 0.95f;
+        }
+        else{
+            rate = 1.0f;
         }
         
-        if (price >= 300000){
-            return (int)(price * 0.9f);
-        }
-        
-        if (price >= 100000){
-            return (int)(price * 0.95f);
-        }
-        
-        
-        return price;
+        */
+        float rate = price >= 500000 ? 0.8f : price >= 300000 ? 0.9f : price >= 100000 ? 0.95f : 1.0f;
+        return (int)(price * rate);
     }
 }
