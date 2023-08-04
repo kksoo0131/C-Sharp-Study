66ae508e6d826ff71ce6f946cc388e712cc48184
김광수 <a67636521@gmail.com>
2023-08-04 15:23:21 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.21 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.cs
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -2,6 +2,6 @@ using System;
 
 public class Solution {
     public int solution(string str1, string str2) {
-       return str1.Contains(str2) ? 1 : 2;
+        return str1.Contains(str2) ? 1 : 2;;
     }
 }
