5825fa30e2b050634312d72e74a1bb4934667e39
김광수 <a67636521@gmail.com>
2023-08-04 15:40:10 +0900

    [level 0] Title: 중앙값 구하기, Time: 1.38 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.cs
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public int solution(int[] array) {
-        return array.OrderBy(x => x).ToArray()[array.Length/2];
+        Array.Sort(array);
+        
+        return array[array.Length/2];
     }
 }
