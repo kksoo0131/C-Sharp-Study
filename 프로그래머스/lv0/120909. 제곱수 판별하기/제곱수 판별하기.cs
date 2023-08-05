952b49e0601a1160e972d35ccdebaf21249fae71
김광수 <a67636521@gmail.com>
2023-08-05 17:41:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cs
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -1,9 +1,8 @@
 using System;
 
 public class Solution {
-    public int solution(int n) {   
-        // 1로 나눴을때 소숫점이 있다면 제곱수가 아님
-        return Math.Sqrt(n) % 1 == 0 ? 1 : 2;
+    public int solution(int n) {
         
+        return Math.Sqrt(n) == (int)Math.Sqrt(n) ? 1 : 2;;
     }
 }
