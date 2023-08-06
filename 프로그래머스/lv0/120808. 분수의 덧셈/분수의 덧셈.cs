9afd6e45cd01493ddf9c00ad9cd0881b5f20d06d
김광수 <a67636521@gmail.com>
2023-08-06 18:43:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.cs
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.cs
@@ -1,32 +1,26 @@
 using System;
 
 public class Solution {
-    static int gcd(int max,int min)
-    {
-        if (min == 0)
-            return max;    
-        return gcd(min, max%min);        
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {      
+        int numer3 = numer1 * denom2 + numer2 *denom1;
+        int denom3 = denom1 * denom2;
+        
+        int gcdInt = gcd(Math.Max(numer3, denom3), Math.Min(numer3, denom3));
+        return new int[] {numer3 / gcdInt, denom3 / gcdInt};
     }
+    
+    int gcd (int a, int b){
+        // gcd(유클리드 호제법) a와 b의 최대공약수는 b와 a%b의 최대공약수임
         
-    public int[] solution(int numer1, int denom1, int numer2, int denom2) 
-    {
-        int[] answer = new int[] {numer1 * denom2 + numer2 * denom1, denom1 * denom2};
-        int r = 1;
-
-        if (answer[0] > answer[1])
-        {
-            r = gcd(answer[0], answer[1]);
-        }
-        else 
-        { 
-            r = gcd(answer[1], answer[0]);
-        }            
-        
-        
-        answer[0] /= r;
-        answer[1] /= r;
+        // 나누어 떨어진다면?
+        if (a % b == 0)
+            return b;
             
-
-        return answer;
+            
+        return gcd(b, a%b);
     }
+    
+    // 1. 두 분수르 더한다
+    // 2. 분자와 분모의 최대공약수를 구한다.
+    
 }
