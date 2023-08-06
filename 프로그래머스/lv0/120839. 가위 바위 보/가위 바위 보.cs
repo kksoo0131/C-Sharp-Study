6ba70c5e99bc486a8d2c59e70a4eca499d38d4fd
김광수 <a67636521@gmail.com>
2023-08-06 18:27:07 +0900

    [level 0] Title: 가위 바위 보, Time: 0.39 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cs
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cs
@@ -1,9 +1,13 @@
 using System;
-using System.Linq;
-
 
 public class Solution {
     public string solution(string rsp) {
-        return String.Concat( rsp.Select( x => x =='2' ? '0' : x == '0' ? '5' : '2'));
+        string answer = "";
+        
+        foreach(char c in rsp){
+            
+            answer += c == '0' ?  5 : c == '2' ? 0 : 2;
+        }
+        return answer;
     }
 }
