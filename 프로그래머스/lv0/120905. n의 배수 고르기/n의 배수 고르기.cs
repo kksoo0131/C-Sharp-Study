104494f157809501ac339ec38a49a0d7d6f8df9e
김광수 <a67636521@gmail.com>
2023-08-11 10:41:20 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.74 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.cs
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -1,16 +1,8 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int n, int[] numlist) {
-        List<int> answer = new List<int>();
-        
-        foreach(int i in numlist){
-            if (i % n == 0){
-                answer.Add(i);
-            }
-            
-        }
-        return answer.ToArray();
+    
+        return Array.FindAll(numlist, x => x%n ==0);
     }
 }
