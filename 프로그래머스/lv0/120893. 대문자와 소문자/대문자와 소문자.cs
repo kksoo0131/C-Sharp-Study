0626b18db5fcc000d52e2eb87ae2bbcbb05c139f
김광수 <a67636521@gmail.com>
2023-08-11 10:14:18 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.34 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cs
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.cs
@@ -1,8 +1,12 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public string solution(string my_string) {
-        return String.Concat(my_string.Select( c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c) ) );
+        string answer = "";
+        
+        foreach (char c in my_string){
+            answer += c < 97 ? Char.ToLower(c) : Char.ToUpper(c);
+        }
+        return answer;
     }
 }
