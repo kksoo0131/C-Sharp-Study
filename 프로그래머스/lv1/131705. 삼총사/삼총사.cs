43fe1cc003a8d2e2fe45c9a87a72302542c92a45
김광수 <a67636521@gmail.com>
2023-08-24 18:43:34 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cs
@@ -1,28 +1,19 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public class Solution {
-    public List <int> answer;
-    public int[] number;
-    
     public int solution(int[] number) {
-        answer = new List<int>();
-        this.number = number;
-        DFS(0,0,0);
-        return answer.Where(x => x==0).Count() ;
-    }
-    
-    public void DFS(int start, int depth, int sum){
+        // 배열에서 3개를 골라서 합이 0이되는 경우의수를 구해라
+        int answer = 0;
         
-        if (depth ==3){ answer.Add(sum); return;}
+        for(int i =0; i< number.Length; i++){
+            for(int j =i+1; j< number.Length; j++){
+                for(int k = j+1 ; k < number.Length; k++){
+                    answer += number[i] + number[j] + number[k] == 0 ? 1 : 0;
+                }
+                
+            }
+        }
         
-        for (int i= start; i<number.Length; i++){
-            sum += number[i];
-            DFS(i+1, depth+1, sum);
-            sum -= number[i];
-            }    
-        
-        
+        return answer;
     }
 }
