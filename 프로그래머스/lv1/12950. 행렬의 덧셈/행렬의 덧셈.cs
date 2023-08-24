2243bb931fd9b5eef31cd2857455647e2716dfbe
김광수 <a67636521@gmail.com>
2023-08-24 17:07:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.81 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2) {
+        for(int i =0; i< arr1.GetLength(0); i++){
+            for(int j = 0; j< arr1.GetLength(1); j++){
+                arr1[i,j] += arr2[i,j];
+            }
+        }
+        return arr1;
+    }
+}
