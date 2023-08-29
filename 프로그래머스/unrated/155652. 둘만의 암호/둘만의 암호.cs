359d2c626b21f8f43fc1b2a3ca2ca5622467f4e5
김광수 <a67636521@gmail.com>
2023-08-29 09:40:21 +0900

    [unrated] Title: 둘만의 암호, Time: 0.40 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.cs
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.cs
@@ -1,24 +1,44 @@
 using System;
 
-public class Solution
-{
-    public string solution(string s, string skip, int index)
-    {
+public class Solution {
+    // 입력받은 문자열 s의 각 자리를 index 만큼 뒤로 밀어줌
+    // skip에 있는 알파벳은 제외
+    
+    public string Skip {get;set;}
+    
+    public string solution(string s, string skip, int index) {
+        Skip = skip;
         string answer = "";
-        foreach ( char c in s)
-        {
-            int start = c;
-           for (int i =0; i < index; i++){
-               
-               start = start + 1;
-               if (start == 123)
-                   start = 97;
-               if (skip.Contains((char)start))
-                   i -=1;               
-           }
-            answer += (char)start;
-        }
         
+        foreach(char c in s)
+        {
+            answer += PushAlphabet(c,index);
+        }
         return answer;
-    } 
+    }
+    // 소문자 아스키 코드 범위는 97~122, 123부터는 -26
+    public char PushAlphabet(char target,int count)
+    {
+        int temp = (int)target;
+        while(count-- > 0)
+        {
+            temp++;
+            if(temp >= 123)
+            {
+                temp -= 26;    
+            }
+            while(Skip.Contains((char)temp))
+            {
+                temp++;
+                if(temp >= 123)
+                {
+                    temp -= 26;    
+                }              
+            }
+
+            
+        }
+
+        return (char)temp;
+    }
 }
