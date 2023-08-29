fd659e19346d4eca8277b320310000a8fba38cc6
김광수 <a67636521@gmail.com>
2023-08-29 09:18:33 +0900

    [unrated] Title: 대충 만든 자판, Time: 96.53 ms, Memory: 35.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.cs
@@ -0,0 +1,70 @@
+using System;
+
+public class Solution {
+    //keymap에 따라서
+    //targets 문자열을 만들기위해서 키를 최소몇번씩 눌러야하는지 순서대로 return
+    // 만들수 없는 경우 -1
+    string[] _keymap;
+    string[] _targets;
+    
+    public int[] solution(string[] keymap, string[] targets) 
+    {
+        
+        _keymap = keymap;
+        _targets = targets;
+        int[] answer = new int[_targets.Length];
+        
+        for(int i =0; i< _targets.Length; i++)
+        {
+            int count = 0;
+            foreach(char c in _targets[i])
+            {
+                if (KeyCheck(c) != -1)
+                {
+                    count += KeyCheck(c);
+                }
+                else
+                {
+                    count = -1;
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+        
+        return answer;
+    }
+    
+    // 키하나를 체크
+    public int KeyCheck(char checkKey)
+    {
+        // keymap을 확인하려는 key로 split했을때
+        // return되는 문자열이 하나면 key가없음
+        // 두개면 앞의 문자열의 길이가 index?
+        // 이러면 근데 제일 앞과 제일뒤에 당첨됬을때 인식 x -> if 문 추가
+        int min = int.MaxValue;
+        foreach(string key in _keymap)
+        {
+            string[] temp = key.Split(checkKey);
+            
+            if(temp.Length > 1)
+            {
+                // 중간 어딘가.
+                 min = Math.Min(temp[0].Length+1, min);
+            }
+            else if (key.Substring(0, key.Length-1) == temp[0])
+            {
+                // 제일 마지막 키일 때
+                min = key.Length;
+            }
+            else if (key.Substring(1, key.Length-1) == temp[0])
+            {
+                // 제일 첫 키일 때  
+                min = 1;
+            }
+            
+        }
+        
+        return min == int.MaxValue ? -1 : min;
+    }
+}
