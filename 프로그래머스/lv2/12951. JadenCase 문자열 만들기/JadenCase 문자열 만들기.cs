5b378dee049dad70e9c4ee695381806606d02919
김광수 <a67636521@gmail.com>
2023-09-04 11:25:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.87 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
@@ -1,12 +1,31 @@
-public class Solution {
-    public string solution(string s) {
-        string[] temp = s.Split(" ");
+
+public class Solution 
+{
+    public string solution(string s) 
+    { 
+        
+        // 공백문자가 연속으로 나왔다면?
+        // splitStr안에 공백문자가 들어가게된다. 그래서 길이가 0인경우를 스킵
+        
+        string[] splitStr = s.Split(" ");
         string answer = "";
         
-        foreach (string ss in temp)
+        foreach(string str in splitStr)
         {
-            answer += ss.Length == 0 ? " " : char.ToUpper(ss[0]) + ss.Substring(1).ToLower()+" ";
+            if (str.Length ==0)
+            {
+                answer += " ";
+                continue;
+            }
+            string temp = str.Substring(0,1);
+            
+            answer += temp.ToUpper();
+            
+            answer += str.Substring(1,str.Length-1).ToLower();
+
+            answer += " ";
         }
-        return answer.Substring(0,answer.Length-1);
+        
+        return answer.Substring(0, answer.Length-1);
     }
 }
