75154b4ed28f86095a34e806d8c5915894d22406
김광수 <a67636521@gmail.com>
2023-09-05 09:20:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 87.01 ms, Memory: 47.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cs
@@ -1,19 +1,69 @@
 using System;
 
-public class Solution {
-    public int[] solution(string s) {
-        int[] answer = new int[] {0,0};
-        while( s != "1"){
-            int preLength = s.Length;
-            s = s.Replace("0", "");
+public class Solution 
+{
+    // 1. 모든 0을 제거
+    // 2. 그때의 길이를 2진법으로 변환
+    // 3. s가 1이 될때까지 반복
+    // 4. return 횟수, 제거된 0의 개수
+    int[] answer = new int[2];
+    public int[] solution(string s) 
+    {
+        while (s != "1")
+        {
+            s = BinaryConversion(s);
+            s = LengthToBinary(s);
+            s = ReverseString(s);
             answer[0] += 1;
-            answer[1] += preLength - s.Length;
-            s = Convert.ToString(s.Length, 2);    
         }
-        
-        
-        
-        
+          
         return answer;
     }
+    
+    public string BinaryConversion(string s)
+    {
+        string temp = "";
+        foreach(char c in s)
+        {
+            if (c == '1')
+            {
+                temp +=c;
+            }
+            else
+            {
+                answer[1] +=1;    
+            }
+        }
+        
+        return temp;
+    }
+    
+    public string LengthToBinary(string s)
+    {
+        int length = s.Length;
+        
+        string binary = "";
+        
+        while(length > 0)
+        {
+            binary += length % 2;
+            length /=2;
+        }
+        
+        return binary;
+        
+    }
+    
+    public string ReverseString(string s)
+    {
+        string reverse = "";
+        
+        for (int i= s.Length-1 ; i >= 0 ; i--)
+        {
+            reverse += s[i];
+        }
+          
+        return reverse;
+    }
+        
 }
