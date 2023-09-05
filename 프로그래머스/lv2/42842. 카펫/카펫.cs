353a3c8411c30daf1e8d2b1820fb56ff5a6db7eb
김광수 <a67636521@gmail.com>
2023-09-05 10:28:22 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.cs
+++ 프로그래머스/lv2/42842. 카펫/카펫.cs
@@ -1,26 +1,68 @@
 using System;
 
 public class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+    // 노랑색 격자의 수는? 가로 -2 * 세로 -2
+    // 갈색 격자의 수는 가로 * 세로 - 노란색 격자
+    
+    // 갈색 격자 + 노란색 격자 = 가로 * 세로
+    
+    // 가로길이가 더길다.
+    
+    public int[] solution(int brown, int yellow) 
+    {
+
         int total = brown + yellow;
-        answer[0] = total/3;
+        int sqrt = (int)Math.Sqrt(total);
         
-        while(true)
+        
+        
+        while(sqrt > 1)
         {
-            if (total % answer[0] == 0)
+            if (total % sqrt == 0)
             {
-                answer[1] = total / answer[0];
-                if ((answer[0] - 2) * (answer[1] - 2) == yellow &&
-                    (answer[0] + answer[1] -2) *2 == brown)
+                int width = total/sqrt;
+                int height = sqrt;
+                
+                if (brown == (width - 2 + height -2) * 2 + 4)
+                {
+                    // 조합이 브라운 갯수를 만족하는지 확인
                     break;
+                }
             }
             
-            answer[0]--;
-                
+            sqrt--;   
+               
         }
         
         
-        return answer;
+        return new int[] {total/sqrt , sqrt};
     }
 }
+
+/* 테스트케이스 4,6,7 실패
+public class Solution {
+    // 노랑색 격자의 수는? 가로 -2 * 세로 -2
+    // 갈색 격자의 수는 가로 * 세로 - 노란색 격자
+    
+    // 갈색 격자 + 노란색 격자 = 가로 * 세로
+    
+    // 가로길이가 더길다.
+    
+    public int[] solution(int brown, int yellow) 
+    {
+        int total = brown + yellow;
+        
+        int sqrt = (int)Math.Sqrt(total);
+        
+        
+        while(total % sqrt != 0)
+        {
+            sqrt--;   
+               
+        }
+        
+        
+        return new int[] {total/sqrt , sqrt};
+    }
+}
+*/
