79973b7f3e5cfdad860e01210cacd68d96aa41e3
김광수 <a67636521@gmail.com>
2023-09-06 09:41:39 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cs
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cs
@@ -2,17 +2,50 @@ using System;
 
 class Solution
 {
+    //  n명이 토너먼트에 참가
+    // 1~n번을 배정 받고 진출한 사람은 다시 1~N/2번을 배정받는다.
+    // 게임은 최종 한명이 남을때 까지 진행된다.
+    // A와 B가 주어질때 A와 B가 언제만날까?
+    
+    // 7 ,9, 27, 33 실패
     public int solution(int n, int a, int b)
     {
-        int answer =0;
-        // 대진표를 한층씩 올라가서 만난다.
-        // a = b 라는 말은 그전 대진에서 a와 b가 만났다는 뜻.
-        while ( a != b){
-            a = a/2 + a%2;
-            b = b/2 + b%2;
+        int answer = 0;
+        
+        while ( a != b)
+        {
+            a = (a + 1) /2;
+            b = (b + 1) /2;    
             answer++;
         }
-        
+
+
         return answer;
     }
 }
+
+/*
+class Solution
+{
+    //  n명이 토너먼트에 참가
+    // 1~n번을 배정 받고 진출한 사람은 다시 1~N/2번을 배정받는다.
+    // 게임은 최종 한명이 남을때 까지 진행된다.
+    // A와 B가 주어질때 A와 B가 언제만날까?
+    
+    // 7 ,9, 27, 33 실패
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        
+        while ( Math.Abs(a-b) > 1)
+        {
+            a = (a + 1) /2;
+            b = (b + 1) /2;    
+            answer++;
+        }
+
+
+        return answer;
+    }
+}
+*/
