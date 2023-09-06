8f2650f1bee623c6fe712749142f9d5f536f80f5
김광수 <a67636521@gmail.com>
2023-09-06 10:01:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -1,20 +1,26 @@
-using System.Collections.Generic;
-public class Solution {
-    Dictionary<int, int> dic = new Dictionary<int, int>();
+public class Solution 
+{
+    // dp문제인거 같은데?
+    // dp[n] = dp[n-2] + dp[n-1]
+    // 피보나치인데 1부터 시작
+    int[] dp;
     
-    public int solution(int n) {    
-        dic.Add(1,1); 
-        dic.Add(2,2); 
-            
-        return fibo(n);    
+    public long solution(int n) 
+    {
+        dp = new int[n+1];
+        
+        if(n < 3){
+            return n;
+        }
+        dp[1] = 1;
+        dp[2] = 2;         
+        
+        
+        for(int i=3; i<= n; i++)
+        {
+            dp[i] = (dp[i-2]+dp[i-1]) % 1234567;
+        }
+        
+        return dp[n];
     }
-    
-    int fibo(int n){
-        if (dic.ContainsKey(n) == false)
-            dic.Add(n, fibo(n-2) + fibo(n-1));
-        return dic[n] % 1234567;
-    }
-    // n에 도착하는 방법은 
-    // n-1 지점에서 한칸 뛰는 경우, n-2 지점에서 두칸 뛰는 경우로 두가지 방법이 있다.
-    // 그러므로 n-1 에 도착하는 방법의 수 + n-2 에 도착하는 방법의 수가 n의 도착하는 방법의 수가 된다.
 }
