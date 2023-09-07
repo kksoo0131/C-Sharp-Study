7215f5f47efddcf88dee41990d1e0f0c427ea6a7
김광수 <a67636521@gmail.com>
2023-09-07 09:01:04 +0900

    [unrated] Title: 귤 고르기, Time: 1502.19 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cs
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cs
@@ -1,30 +1,35 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 
 public class Solution {
-    public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        Dictionary<int, int> dic = new Dictionary<int, int>();
-        
-        foreach (int i in tangerine){
-            if (dic.ContainsKey(i) == false)
-                dic.Add(i, 1);
-            else
-                dic[i] +=1;
+    // 귤을 크기별로 분류하고
+    // 한 상자에 귤의 종류가 최소가 되도록 담는다
+    
+    // 배열에 index == 귤의크기 value == 갯수로 배열을 만든다.
+    // 배열을 정렬해서 종류가 많은 귤부터 상자에 담는다.
+    public int solution(int k, int[] tangerine) 
+    {
+        int[] classify = new int[10000001];
+        int answer =0;
+        foreach(int i in tangerine)
+        {
+            classify[i]++;
         }
         
-       
-        List<int> list = dic.Select(x => x.Value).ToList();
-        list.Sort();
+        Array.Sort(classify);
+        Array.Reverse(classify);
         
-        while(k>0){
-            k -= list[list.Count-1];
-            list.Remove(list[list.Count-1]);
+        foreach(int i in classify)
+        {
+            k -= i;
             answer++;
+            
+            if(k <= 0)
+            {
+                break;
+            }
+            
         }
         
-
         return answer;
     }
 }
