43c49e6d389da0e03b85c23d21f2b67407054cc3
김광수 <a67636521@gmail.com>
2023-09-07 14:58:33 +0900

    [level 2] Title: H-Index, Time: 1.93 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.cs
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cs
@@ -1,26 +1,43 @@
 using System;
 
 public class Solution {
-    public int solution(int[] citations) {
+    // n편의 논문중 h번 이상 인용된 논문이 h편 이상이면 H-Index 는 h
+    
+    // h보다 크거나 같은게 h개 이상이라면
+    
+    // 일단은 citations를 정렬한다.
+    
+    // 그러면 배열의 index는 이 논문보다 적게 인용된 나머지 논문의 개수가 된다.
+    
+    // length - index는 이 논문 이상으로 인용된 논문의 개수가 된다( 이 논문도 포함임)
+    
+    // 만약 citations 배열의 길이가
+    
+    // 요소의 최솟값보다 짧다면 citations의 길이가 answer가 된다.
+    
+    public int solution(int[] citations) 
+    {
         Array.Sort(citations);
-        int answer = 0; // 0 1 3 5 6
-        for (int i = 0; i < 10000; i++){
-            // 배열안에서 i 보다 큰것의 갯수가 i보다 많다면
-           for(int j =0; j< citations.Length; j++)
-           {
-               if (citations[j] >= i && citations.Length -j >= i )
-               {
-                   answer = i;
-                   break;
-               }
-                    
-           }
-            // answer == i가 아니면 틀렸음 return
-            if (answer != i)
-                break;
+        
+        int answer = 0;
+        
+        for (int i =0; i< citations.Length; i++)
+        {
+            
+            if (citations[i] <= citations.Length -i)
+            {
+                if (i <= citations[i])
+                {
+                    answer = citations[i];    
+                }
+                
+            }
+            else
+            {
+                answer =  Math.Max(answer, citations.Length -i);
+            }
         }
         
-    
         return answer;
     }
 }
