26ccb483bc039cc97b8c4b88d4ccb0ae91dbea4f
김광수 <a67636521@gmail.com>
2023-09-07 09:38:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.81 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.cs
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.cs
@@ -2,48 +2,78 @@ using System;
 using System.Collections.Generic;
 
 public class Solution {
-    public int solution(string s) {
+    // x칸 왼쪽으로 밀었을때 괄호문자열이 올바른가?
+    
+    // 왼쪽 괄호면 stack 에넣고
+    // 오른쪽 괄호면 stack의 Peek 을 확인
+    public int solution(string s) 
+    {
         int answer = 0;
-        for(int i=0; i < s.Length; i++){
-            char temp = s[0]; 
-            s = s.Substring(1) + temp.ToString();
-            answer += check(s);
+        
+        for(int i =0; i< s.Length; i++)
+        {
+            answer += PushString(s,i) ? 1 : 0;
         }
         
         return answer;
     }
-
-    int check (string s){
-        int small = 0;
-        int middle = 0;
-        int large = 0;
-        Stack<char> stack = new Stack<char>();
-        //마지막으로 열린게 먼저 닫혀야한다.
-        // 열린거 stack으로 관리해줘야됨
+    
+    public bool PushString(string s, int i)
+    {
+        string str = s.Substring(i, s.Length-i) + s.Substring(0, i);
+        Stack<char> _stack = new Stack<char>();
         
-        foreach(char c in s){
-            if (c == '(' || c == '{' || c == '[')
-                stack.Push(c);
+        foreach(char c in str)
+        {
+            if (LeftString(c))
+            {
+                _stack.Push(c);
+            }
+            else if (_stack.Count != 0 && MatchString(_stack.Peek(), c))
+            {
+                _stack.Pop();
+            }
             else
             {
-                if (stack.Count == 0)
-                    return 0;
-                
-                char temp = stack.Peek();
-                
-                if (temp == '(' && c == ')')
-                    stack.Pop();
-                else if (temp == '{' && c == '}')
-                    stack.Pop();
-                else if (temp == '[' && c == ']')
-                    stack.Pop();
-                else
-                    return 0;
-                
+                return false;
             }
+            
         }
-        if (stack.Count != 0)
-            return 0;
-        return 1;
-    }               
+        
+        // 남아있으면 올바르지않음
+        
+        
+        return _stack.Count > 0 ? false :true;        
+    }
+    
+    public bool LeftString(char a)
+    {
+        switch(a)
+        {
+            case '{':
+                return true;
+            case '[':
+                return true;
+            case '(':
+                return true;
+            default :
+                return false;    
+        }
+        
+    }
+    
+    public bool MatchString(char a, char b)
+    {
+        switch(a)
+        {
+            case '{':
+                return b == '}' ? true : false;
+            case '[':
+                return b == ']' ? true : false;
+            case '(':
+                return b == ')' ? true : false;
+            default :
+                return false;    
+        }
+    }
 }
