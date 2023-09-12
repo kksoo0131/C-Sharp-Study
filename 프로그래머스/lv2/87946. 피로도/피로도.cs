f8fb0e87ac079081c15189437b3c0d7bd46076f1
김광수 <a67636521@gmail.com>
2023-09-12 16:17:28 +0900

    [level 2] Title: 피로도, Time: 0.77 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.cs
+++ 프로그래머스/lv2/87946. 피로도/피로도.cs
@@ -2,22 +2,74 @@ using System;
 using System.Collections.Generic;
 
 public class Solution {
-    // 최소 피로도, 소모 피로도
-    // 던전 리스트를 순회하면서 탐험할 수 있는 최대 던전수를 return
+    // 길이체크를 앞에서 하면 안되는 이유
     
-    // 던전 탐험의 순서에 따라서 결과가 달라진다.
+    // 만약 이번 선택을 하고나면 남은 모든 던전은 갈수 없을때
     
-    // 최소 피로도가 크고 소모 피로도가 작을수록 우선순위가 높아야함
+    // 길이체크를 앞에서하게되면  num이 업데이트 되지않아서 num을 업데이트 하지않고 함수가 종료됨
     
-    // 던전의 개수는 8이하임 모든 순서를 고려해도 될듯?
+    // 원래 처음에 길이체크를 앞에서 한 이유는 인덱스 범위를 벗어날까봐였는데
     
+    // 생각해보니 반복문을 통해서 인덱스의 길이를 한번체크하기때문에 그렇게할 필요가없었다.
+    int answer = 0;
+    public int solution(int k, int[,] dungeons) 
+    {   
+        Queue<int[]> queue = new Queue<int[]>();
+        
+        for(int i =0; i< dungeons.GetLength(0); i++)
+        {
+            queue.Enqueue(new int[]{dungeons[i,0], dungeons[i,1]});
+        }
+        
+        RecursiveDungeon(0, k, queue);
+        
+        return answer;
+        
+    }
     
-    // n개의 던전에서 최소 피로도가 k보다 낮은 던전중 하나를 선택해서 탐험.
-    
-    // 재귀, 필요한것 1. 현재 피로도, 2. 남은 던전 리스트, 3. 탐험한 던전의 수
-    
-    // 최소피로도를 만족못하는것들은 버림
-    
+    public void RecursiveDungeon(int num,int k, Queue<int[]> queue)
+    {
+        Queue<int[]> tempQueue = new Queue<int[]>();
+        
+        // queue를 깊은복사.
+        foreach (int[] arr in queue)
+        {
+            tempQueue.Enqueue(arr);
+        }
+               
+        int length = tempQueue.Count;
+        
+        // queue의 요소 갯수만큼 반복
+        // 다음 재귀에는 각각의 요소를 선택하고 남은 큐를 복사해서 사용하게됨
+        for(int i =0; i <length; i++)
+        {
+            int[] temp = tempQueue.Dequeue();
+            
+            if(temp[0] <= k)
+            {
+                RecursiveDungeon(num+1, k-temp[1], tempQueue);
+                tempQueue.Enqueue(temp);
+            }
+        }
+        
+         
+         // 길이가 0이라면 끝
+        if(tempQueue.Count == 0)
+        {
+            answer = Math.Max(answer, num);
+            return;
+        }
+        
+       
+        
+    }
+}
+
+/*
+using System;
+using System.Collections.Generic;
+
+public class Solution {
     int answer = 0;
     public int solution(int k, int[,] dungeons) 
     {   
@@ -44,13 +96,13 @@ public class Solution {
             tempQueue.Enqueue(arr);
         }
         
-        // 길이가 0이라면 끝
+                 // 길이가 0이라면 끝
         if(tempQueue.Count == 0)
         {
             answer = Math.Max(answer, num);
             return;
         }
-        
+               
         int length = tempQueue.Count;
         
         // queue의 요소 갯수만큼 반복
@@ -66,12 +118,8 @@ public class Solution {
             }
         }
         
-               // 길이가 0이라면 끝
-        if(tempQueue.Count == 0)
-        {
-            answer = Math.Max(answer, num);
-            return;
-        }
+       
         
     }
 }
+*/
